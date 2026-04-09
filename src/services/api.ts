d7c50b64363b05c490e8d34dce6147eb86963e3c
Jahir1807 <jonaja0718@gmail.com>
2026-04-08 23:15:02 -0600

    se me olvido el back jeje

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 export const BASE_URL =
-  (import.meta.env.VITE_API_URL as string) || "https://localhost:7197";
+  (import.meta.env.VITE_API_URL as string) || "https://sistemapolizasegresostulaallende.somee.com";
 
 type AuthStored = { token?: string; Token?: string };
 
